8704b7dfcfe25c75634b22ef13825d4be6088fd0
김민주 <ducco705@snu.ac.kr>
2024-02-21 17:58:32 +0900

    🚧 기본 UI 구현

---
+++ flutter_book_mall/lib/screens/screen_index.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_book_mall/tabs/tab_cart.dart';
+import 'package:flutter_book_mall/tabs/tab_home.dart';
+import 'package:flutter_book_mall/tabs/tab_profile.dart';
+import 'package:flutter_book_mall/tabs/tab_search.dart';
+
+class IndexScreen extends StatefulWidget {
+  const IndexScreen({super.key});
+
+  @override
+  State<IndexScreen> createState() => _IndexScreenState();
+}
+
+class _IndexScreenState extends State<IndexScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _tabs = [
+    HomeTab(),
+    SearchTab(),
+    CartTab(),
+    ProfileTab(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          iconSize: 30,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: TextStyle(fontSize: 12),
+          currentIndex: _currentIndex,
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: '장바구니'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: '프로필'),
+          ],
+        ),
+      body: _tabs[_currentIndex],
+    );
+  }
+}
