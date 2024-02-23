34171fd6208f2d45e7e4189e1d58078bdde63294
김민주 <ducco705@snu.ac.kr>
2024-02-23 10:44:23 +0900

    ✨ autologin(remember me)

--- flutter_book_mall/lib/screens/screen_splash.dart
+++ flutter_book_mall/lib/screens/screen_splash.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_book_mall/models/model_auth.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,8 +15,23 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   Future<bool> checkLogin() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
+    final authClient = Provider.of<FirebaseAuthProvider>(context, listen: false);
     bool isLogin = prefs.getBool('isLogin') ?? false;
     print("[*] 로그인 상태 : " + isLogin.toString());
+    if(isLogin){
+      String? email = prefs.getString('email');
+      String? password = prefs.getString('password');
+      print("[*} 저장된 정보로 로그인 시도");
+      await authClient.loginWithEmail(email!, password!).then((loginStatus){
+        if(loginStatus == AuthStatus.loginSuccess){
+          print("[+] 로그인 성공");
+        }else{
+          print("[-] 로그인 실패");
+          isLogin = false;
+          prefs.setBool('isLogin', false);
+        }
+      });
+    }
     return isLogin;
   }
 
@@ -31,14 +48,19 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState(){
     super.initState();
-    Timer(Duration(milliseconds: 1500), (){
+    Timer(const Duration(milliseconds: 1500), (){
       moveScreen();
     });
   }
 
   @override
+  void dispose(){
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: null,
       body: Center(
         child: Text('Splash Screen'),
